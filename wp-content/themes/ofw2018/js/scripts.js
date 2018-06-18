43e787a2123192bb743093feeb69bad150554d8c
nogarulab <admin@nogarulab.com>
2018-06-18 09:42:35 -0500

    first commit

---
+++ wp-content/themes/ofw2018/js/scripts.js
@@ -0,0 +1,20 @@
+(function ($, root, undefined) {
+  
+  $(window).on('load', function() { // makes sure the whole site is loaded 
+	  $('#status').fadeOut(); // will first fade out the loading animation 
+	  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	  $('body').delay(350).css({'overflow':'visible'});
+	});
+	
+	$(function () {
+		
+		'use strict';
+		
+		// DOM ready, take it away
+		
+		$("html").easeScroll();
+    
+    
+	});
+	
+})(jQuery, this);
---
+++ wp-content/themes/ofw2018/page.php
@@ -0,0 +1,78 @@
+<?php 
+
+get_header(); 
+
+if ( is_page('account') ) {
+  
+  if ( current_user_can('agent') ) :
+    
+    //Earl uni na ang magiging dashboard kan agents after sya mag login
+    echo 'agent here';
+    echo '<a href="'. wp_logout_url( home_url() ) .'">Logout</a>';
+  
+  elseif ( current_user_can('partner') ) :
+  
+    echo 'partner here';
+    echo '<a href="'. wp_logout_url( home_url() ) .'">Logout</a>';
+  
+  else :
+  
+    echo do_shortcode('[ms-membership-account]');
+    echo '<hr>';
+    echo do_shortcode('[ms-membership-logout]');
+  
+  endif;
+  
+} else {
+
+?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<h1><?php the_title(); ?></h1>
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php the_content(); ?>
+
+				<?php comments_template( '', true ); // Remove if you don't want comments ?>
+
+				<br class="clear">
+
+				<?php edit_post_link(); ?>
+
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php 
+        
+}
+
+get_footer();
+
+?>

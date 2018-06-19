76379077f4338d0d744efd0e76df4dd1abdafa5f
jesel@sakurasky.com <jesel@sakurasky.com>
2018-06-20 01:33:08 +0800

    test

--- wp-content/themes/ofw2018/page.php
+++ wp-content/themes/ofw2018/page.php
@@ -19,7 +19,6 @@ if ( is_page('partner-dashboard') ) {
 
 	if ( is_user_logged_in() && current_user_can('agent') ) :
 
-		get_template_part( 'partners/form', get_post_format() );
 		echo '<a href="'. wp_logout_url( home_url() ) .'">Logout</a>';
 
 	else :
@@ -38,7 +37,11 @@ if ( is_page('partner-dashboard') ) {
 
 			<h1><?php the_title(); ?></h1>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<?php 
+
+			if (have_posts()): 
+				while (have_posts()) : 
+					the_post(); ?>
 
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -54,27 +57,36 @@ if ( is_page('partner-dashboard') ) {
 			</article>
 			<!-- /article -->
 
-		<?php endwhile; ?>
+		<?php 
+				endwhile;
+			else:
 
-		<?php else: ?>
+		?>
 
 			<!-- article -->
 			<article>
-
 				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
 			</article>
 			<!-- /article -->
 
-		<?php endif; ?>
+		<?php 
+
+			endif;
+
+			if ( is_page('request-for-partnership') ) {
+				get_template_part( 'partners/form', get_post_format() );
+			}
+
+
+		?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
-
 <?php 
+
+	get_sidebar();
         
 }
 
--- wp-content/themes/ofw2018/partners/form.php
+++ wp-content/themes/ofw2018/partners/form.php
@@ -1,1 +1,104 @@
-this is a form
+<?php 
+
+if ( ! is_user_logged_in() ) :
+
+	global $wpdb, $user_ID;  
+	if ($user_ID) {  
+	   
+	    // They're already logged in, so we bounce them back to the homepage.  
+	    header( 'Location:' . home_url() );  
+	   
+	} else {  
+	   
+	    $errors = array();  
+	   
+	    if( $_SERVER['REQUEST_METHOD'] == 'POST' ) 
+	      {  
+	   
+	        // Check username is present and not already in use  
+	        $username = $wpdb->escape($_REQUEST['username']);  
+	        if ( strpos($username, ' ') !== false )
+	        {   
+	            $errors['username'] = "Sorry, no spaces allowed in usernames";  
+	        }  
+	        if(emptyempty($username)) 
+	        {   
+	            $errors['username'] = "Please enter a username";  
+	        } elseif( username_exists( $username ) ) 
+	        {  
+	            $errors['username'] = "Username already exists, please try another";  
+	        }  
+	   
+	        // Check email address is present and valid  
+	        $email = $wpdb->escape($_REQUEST['email']);  
+	        if( !is_email( $email ) ) 
+	        {   
+	            $errors['email'] = "Please enter a valid email";  
+	        } elseif( email_exists( $email ) ) 
+	        {  
+	            $errors['email'] = "This email address is already in use";  
+	        }  
+	   
+	        // Check password is valid  
+	        if(0 === preg_match("/.{6,}/", $_POST['password']))
+	        {  
+	          $errors['password'] = "Password must be at least six characters";  
+	        }  
+	   
+	        // Check password confirmation_matches  
+	        if(0 !== strcmp($_POST['password'], $_POST['password_confirmation']))
+	         {  
+	          $errors['password_confirmation'] = "Passwords do not match";  
+	        }  
+	   
+	        // Check terms of service is agreed to  
+	        if($_POST['terms'] != "Yes")
+	        {  
+	            $errors['terms'] = "You must agree to Terms of Service";  
+	        }  
+	   
+	        if(0 === count($errors)) 
+	         {  
+	   
+	            $password = $_POST['password'];  
+	   
+	            $new_user_id = wp_create_user( $username, $password, $email );  
+	   
+	            // You could do all manner of other things here like send an email to the user, etc. I leave that to you.  
+	   
+	            $success = 1;  
+	   
+	            //header( 'Location:' . get_bloginfo('url') . '/login/?success=1&u=' . $username );  
+	   
+	        }  
+	   
+	    }  
+	}  
+
+?>
+
+<form id="wp_signup_form" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">  
+  
+    <div>
+        
+        <div>
+        	<input type="text" name="email" id="email">
+        	<i>This will also be your username.</i>
+        </div>
+        <div><input type="password" name="password" id="password">  </div>
+        <div><input type="text" name="firstname" id="firstname">  </div>
+        <!-- <label for="password_confirmation">Confirm Password</label>  
+        <input type="password" name="password_confirmation" id="password_confirmation">   -->
+  
+        <!-- <input name="terms" id="terms" type="checkbox" value="Yes">  
+        <label for="terms">I agree to the Terms of Service</label>   -->
+        <input type="submit" id="submitbtn" name="submit" value="Sign Up" />  
+    </div>
+  
+</form>  
+
+<?php
+else :
+	echo "You already have an account, you don't need to create another.";
+endif;
+?>

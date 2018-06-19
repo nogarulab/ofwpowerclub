b2c6ea577ca7a9e688ef27e9b1b159e45d180be5
jesel@sakurasky.com <jesel@sakurasky.com>
2018-06-20 07:59:30 +0800

    adding new field

--- wp-content/themes/ofw2018/partners/form.php
+++ wp-content/themes/ofw2018/partners/form.php
@@ -1,6 +1,6 @@
 <?php 
 
-if ( ! is_user_logged_in() ) :
+//if ( ! is_user_logged_in() ) :
 
 	global $wpdb, $user_ID;  
 	if ($user_ID) {  
@@ -16,21 +16,21 @@ if ( ! is_user_logged_in() ) :
 	      {  
 	   
 	        // Check username is present and not already in use  
-	        $username = $wpdb->escape($_REQUEST['username']);  
-	        if ( strpos($username, ' ') !== false )
-	        {   
-	            $errors['username'] = "Sorry, no spaces allowed in usernames";  
-	        }  
-	        if(emptyempty($username)) 
-	        {   
-	            $errors['username'] = "Please enter a username";  
-	        } elseif( username_exists( $username ) ) 
-	        {  
-	            $errors['username'] = "Username already exists, please try another";  
-	        }  
+	        // $username = $wpdb->escape($_REQUEST['username']);  
+	        // if ( strpos($username, ' ') !== false )
+	        // {   
+	        //     $errors['username'] = "Sorry, no spaces allowed in usernames";  
+	        // }  
+	        // if(emptyempty($username)) 
+	        // {   
+	        //     $errors['username'] = "Please enter a username";  
+	        // } elseif( username_exists( $username ) ) 
+	        // {  
+	        //     $errors['username'] = "Username already exists, please try another";  
+	        // }  
 	   
 	        // Check email address is present and valid  
-	        $email = $wpdb->escape($_REQUEST['email']);  
+	        $email = esc_sql($_REQUEST['email']);  
 	        if( !is_email( $email ) ) 
 	        {   
 	            $errors['email'] = "Please enter a valid email";  
@@ -52,17 +52,25 @@ if ( ! is_user_logged_in() ) :
 	        }  
 	   
 	        // Check terms of service is agreed to  
-	        if($_POST['terms'] != "Yes")
-	        {  
-	            $errors['terms'] = "You must agree to Terms of Service";  
-	        }  
+	        // if($_POST['terms'] != "Yes")
+	        // {  
+	        //     $errors['terms'] = "You must agree to Terms of Service";  
+	        // }
 	   
 	        if(0 === count($errors)) 
-	         {  
+	        {  
 	   
 	            $password = $_POST['password'];  
 	   
-	            $new_user_id = wp_create_user( $username, $password, $email );  
+	            $new_user_id = wp_create_user( $email, $password, $email );
+	            $user = new WP_User($new_user_id);
+	            $user->set_role('partner');
+	            $display_name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+
+	            wp_update_user( array ('ID' => $new_user_id,  'display_name' => $display_name) ) ;
+	            update_user_meta( $new_user_id, 'first_name', sanitize_text_field( $_POST['firstname'] ) );
+	            update_user_meta( $new_user_id, 'last_name', sanitize_text_field( $_POST['lastname'] ) );
+
 	   
 	            // You could do all manner of other things here like send an email to the user, etc. I leave that to you.  
 	   
@@ -70,7 +78,11 @@ if ( ! is_user_logged_in() ) :
 	   
 	            //header( 'Location:' . get_bloginfo('url') . '/login/?success=1&u=' . $username );  
 	   
-	        }  
+	        }  else {
+	        	foreach ($errors as $error) {
+	        		echo "<div>".$error."</div>";
+	        	}
+	        }
 	   
 	    }  
 	}  
@@ -79,26 +91,22 @@ if ( ! is_user_logged_in() ) :
 
 <form id="wp_signup_form" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">  
   
-    <div>
         
-        <div>
-        	<input type="text" name="email" id="email">
-        	<i>This will also be your username.</i>
-        </div>
-        <div><input type="password" name="password" id="password">  </div>
-        <div><input type="text" name="firstname" id="firstname">  </div>
-        <!-- <label for="password_confirmation">Confirm Password</label>  
-        <input type="password" name="password_confirmation" id="password_confirmation">   -->
-  
+        <!-- <input type="text" name="username" id="username">   -->
+        <div><input type="text" name="email" id="email" placeholder="Email Address"></div>
+        <div><input type="password" name="password" id="password" placeholder="Password">  </div>
+        <div><input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">  </div>
+        <div><input type="text" name="firstname" id="firstname" placeholder="First Name"></div>
+        <div><input type="text" name="lastname" id="lastname" placeholder="Last Name">  </div>
         <!-- <input name="terms" id="terms" type="checkbox" value="Yes">  
         <label for="terms">I agree to the Terms of Service</label>   -->
-        <input type="submit" id="submitbtn" name="submit" value="Sign Up" />  
-    </div>
   
-</form>  
+        <div><input type="submit" id="submitbtn" name="submit" value="Sign Up" />  </div>
+  
+</form> 
 
 <?php
-else :
-	echo "You already have an account, you don't need to create another.";
-endif;
+// else :
+// 	echo "You already have an account, you don't need to create another.";
+// endif;
 ?>

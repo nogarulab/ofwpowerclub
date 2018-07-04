2ca4e6dbc411116146b29ce7021542600f8e6105
jesel@sakurasky.com <jesel@sakurasky.com>
2018-07-04 23:29:02 +0800

    test

--- wp-content/themes/ofw2018/partners/member-search.php
+++ wp-content/themes/ofw2018/partners/member-search.php
@@ -1,6 +1,15 @@
-
+<div class="partner-dashboard-home">
+	<header>
+		<h3>Member Search</h3>
+		<p>Verify customers if they are OFW Power Club members by searching them here. Please enter full name or email address.</p>
+	</header>
 <?php
 if ( is_user_logged_in() && current_user_can('partner') ) :
+?>
+	<form action="<?php echo home_url(); ?>/member-search?ms=<?php echo $s_query; ?>" method="GET" class="member-search">
+		<div><input type="text" name="ms" value="" class="form-control" placeholder="Name or Email Address" /><input type="submit" value="Search"></div>
+	</form>
+<?php
 	if (isset($_GET['ms'])) {
 		$m_name = '*'.$_GET['ms'].'*';
 	} else {
@@ -17,22 +26,27 @@ if ( is_user_logged_in() && current_user_can('partner') ) :
 	$user_query = new WP_User_Query( $args );
 
     $users = $user_query->get_results();
+    //print_r($users);
 
     if (isset($_GET['ms'])) {
-    	//print_r($users);
+    	echo '<div class="search-results"><h4>Results</h4><ul class=" row">';
     	foreach ($users as $user) {
-    		echo $user->display_name;
+    		$user_id = $user->ID;
+    		echo '<li class="col-lg-6"><div>';
+    		echo '<img src="'.get_template_directory_uri().'/img/no-profile-pic.png" alt="'.$user->display_name.'">';
+    		echo '<strong>'.$user->display_name.'</strong> ';
+    		echo '<em>'.$user->user_email.'</em> ';
+    		echo '<span>'.get_user_meta($user_id, 'id_number', true).'</span>';
+    		echo '</div></li>';
     	}
+    	echo '</ul></div>';
     }
 
     $s_query = isset($_POST['ms']);
 
 ?>
 
-	<form action="<?php echo home_url(); ?>/member-search?ms=<?php echo $s_query; ?>" method="GET">
-		<input type="text" name="ms" value="" />
-    	<input type="submit" value="Search">
-	</form>
+	
 
 <?php
 
@@ -40,3 +54,4 @@ else :
 	echo '<p>You do not have permission to use this form!</p>';
 endif;
 ?>
+</div>

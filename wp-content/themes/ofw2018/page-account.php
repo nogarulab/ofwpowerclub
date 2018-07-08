364848a217237ec3695c5fe47b6126cf538c8a6a
jujujuanna <joanastronomo@gmail.com>
2018-07-08 17:39:20 +0800

    account template

---
+++ wp-content/themes/ofw2018/page-account.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: Account */  ?>
+
+<?php get_header();  ?>
+
+<section class="py-5">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 text-center">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer();  ?>

6bbdd6646b4480f29723e169889fd78d38b0fe2f
jujujuanna <joanastronomo@gmail.com>
2018-07-08 17:58:54 +0800

    login form style 1

---
+++ wp-content/themes/ofw2018/scss/_forms.php
@@ -0,0 +1,29 @@
+
+.ms-membership-form-wrapper {
+	legend {
+		font-size: 2em;
+	    font-weight: 600;
+	    text-transform: uppercase;
+	    border-bottom: 0;
+	}
+	.ms-alert-error {
+		border-top-color: $red;
+	}
+	input {
+		display: block;
+	    width: 100%;
+	    padding: .375rem .75rem;
+	    font-size: 1rem;
+	    line-height: 1.5;
+	    color: #495057;
+	    background-color: #fff;
+	    background-clip: padding-box;
+	    border: 1px solid #ced4da;
+	    border-radius: .25rem;
+	}
+	.ms-login-form {
+		max-width: 500px;
+		display: block;
+		margin: auto;
+	}
+}

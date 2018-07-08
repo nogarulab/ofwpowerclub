4706e71bd690dbecbf43f206a275c8008de20e1b
jujujuanna <joanastronomo@gmail.com>
2018-07-08 17:46:16 +0800

    query content account page

--- wp-content/themes/ofw2018/page-account.php
+++ wp-content/themes/ofw2018/page-account.php
@@ -6,8 +6,13 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-12 text-center">
-				<?php the_content(); ?>
-			</div>
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+				<?php else: ?>	
+					<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+				<?php endif; ?>
+			</div>	
 		</div>
 	</div>
 </section>

00f4622a043c71b72c425319b87a1de5ae42a5ff
jesel@sakurasky.com <jesel@sakurasky.com>
2018-07-09 09:34:58 +0800

    displaying the errors

--- wp-content/themes/ofw2018/agents/addmember.php
+++ wp-content/themes/ofw2018/agents/addmember.php
@@ -236,7 +236,11 @@ if ( is_user_logged_in() && current_user_can( 'agent' ) ) :
 
         } else {
 
-        	echo 'Errors';
+        	echo '<ul>';
+        	foreach ($errors as $error) {
+        		echo '<li>'.$error.'</li>';
+        	}
+        	echo '</ul>';
 
         }
 

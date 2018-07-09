c79fb8fe249d9927a3f46ef09001928be3f475f0
jesel@sakurasky.com <jesel@sakurasky.com>
2018-07-09 10:33:05 +0800

    fixing the member search results

--- wp-content/themes/ofw2018/partners/member-search.php
+++ wp-content/themes/ofw2018/partners/member-search.php
@@ -24,22 +24,26 @@ if ( is_user_logged_in() && current_user_can('partner') ) :
 				'number'			=> -1
 	        );
 	$user_query = new WP_User_Query( $args );
-
+	$user_found = $user_query->get_total();
     $users = $user_query->get_results();
     //print_r($users);
 
     if (isset($_GET['ms'])) {
-    	echo '<div class="search-results"><h4>Results</h4><ul class=" row">';
-    	foreach ($users as $user) {
-    		$user_id = $user->ID;
-    		echo '<li class="col-lg-6"><div>';
-    		echo '<img src="'.get_template_directory_uri().'/img/no-profile-pic.png" alt="'.$user->display_name.'">';
-    		echo '<strong>'.$user->display_name.'</strong> ';
-    		echo '<em>'.$user->user_email.'</em> ';
-    		echo '<span>'.get_user_meta($user_id, 'id_number', true).'</span>';
-    		echo '</div></li>';
-    	}
-    	echo '</ul></div>';
+    	if ($user_found != 0) :
+	    	echo '<div class="search-results"><h4>Results</h4><ul class=" row">';
+	    	foreach ($users as $user) {
+	    		$user_id = $user->ID;
+	    		echo '<li class="col-lg-6"><div>';
+	    		echo '<img src="'.get_template_directory_uri().'/img/no-profile-pic.png" alt="'.$user->display_name.'">';
+	    		echo '<strong>'.$user->display_name.'</strong> ';
+	    		echo '<em>'.$user->user_email.'</em> ';
+	    		echo '<span>'.get_user_meta($user_id, 'id_number', true).'</span>';
+	    		echo '</div></li>';
+	    	}
+	    	echo '</ul></div>';
+	    else :
+	    	echo '<p>No member found.</p>';
+	    endif;
     }
 
     $s_query = isset($_POST['ms']);

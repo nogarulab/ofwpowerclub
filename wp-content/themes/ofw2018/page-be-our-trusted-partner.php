5e4179b857b7d86607e9a6c64a015d868aff9814
jujujuanna <joanastronomo@gmail.com>
2018-07-11 23:20:47 +0800

    button

--- wp-content/themes/ofw2018/page-be-our-trusted-partner.php
+++ wp-content/themes/ofw2018/page-be-our-trusted-partner.php
@@ -26,7 +26,7 @@ get_header();
 			<div class="col-12">
 				<section class="py-5">
 					<?php the_content(); ?>
-					<a href="<?php echo home_url(); ?>/request-for-partnership" class="h-c-white partner-link bg-blue white text-uppercase py-2 px-4">Be A Partner</a>
+					<a href="<?php echo home_url(); ?>/request-for-partnership" class="h-c-white rounded partner-link bg-blue white text-uppercase py-3 px-5">Be A Partner</a>
 				</section>
 			</div>
 		</div>

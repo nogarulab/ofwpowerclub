7f1a434deb7030a1e7f3129a40c31624630e1cf7
jujujuanna <joanastronomo@gmail.com>
2018-07-12 22:50:27 +0800

    website link

--- wp-content/themes/ofw2018/partners/single-partner-template.php
+++ wp-content/themes/ofw2018/partners/single-partner-template.php
@@ -18,7 +18,11 @@
 							<div class="col-lg-9 col-md-10 ml-auto">
 								<h2 class="text-uppercase font-weight-bold blue">About <?php the_title(); ?></h2>
 								<?php the_content(); ?>
-								<a href="<?php echo get_post_meta(get_the_ID(), 'establishmentwebsite', true); ?>" class="white transition h-c-white rounded bg-red py-3 px-5 d-inline-block mt-3">Visit our Website</a>
+
+								<?php if (get_post_meta(get_the_ID(), 'establishmentwebsite', true);): ?>
+									<a href="<?php echo get_post_meta(get_the_ID(), 'establishmentwebsite', true); ?>" class="white transition h-c-white rounded bg-red py-3 px-5 d-inline-block mt-3">Visit our Website</a>
+								<?php endif ?>
+
 							</div>
 						</div>
 					</div>

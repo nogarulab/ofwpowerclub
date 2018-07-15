4bd95f1431c737be49d21f62d45f89f1a9a76cb0
jujujuanna <joanastronomo@gmail.com>
2018-07-15 19:05:34 +0800

    autoscroll

--- wp-content/themes/ofw2018/agents/agent-dashboard-menu.php
+++ wp-content/themes/ofw2018/agents/agent-dashboard-menu.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="<?php echo home_url(); ?>/agent-dashboard">Dashboard</a></li>
 		<li><a href="<?php echo home_url(); ?>/add-a-member">Add A Member</a></li>
-		<li><a href="<?php echo home_url(); ?>/agent-dashboard?showform=1" class="change-pass">Change Password</a></li>
+		<li><a href="<?php echo home_url(); ?>/agent-dashboard?showform=1#changepass" class="change-pass">Change Password</a></li>
 		<li><a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
 	</ul>
 </div>

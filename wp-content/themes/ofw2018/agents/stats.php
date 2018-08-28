428917608d3aae76d80870f6cd9950d14684f41f
jesel@sakurasky.com <jesel@sakurasky.com>
2018-08-29 05:26:26 +0800

    stat fixing

--- wp-content/themes/ofw2018/agents/stats.php
+++ wp-content/themes/ofw2018/agents/stats.php
@@ -454,7 +454,9 @@
     echo '<div>Total Users That Are Member For This Year By Agent ID '.$thisUser.' = '.count($members_for_this_year).'</div>';
 
 ?>
-
+<div>
+    <?php print_r($all_registered_user_ids_list); ?>
+</div>
 <div class="points">
     <div class="row">
         <div class="col-lg-6">

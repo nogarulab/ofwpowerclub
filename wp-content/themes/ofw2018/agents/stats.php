59908c0790e97a3c74f152f7ff885516e4209ad8
jesel@sakurasky.com <jesel@sakurasky.com>
2018-08-29 00:24:15 +0800

    stat fixing

--- wp-content/themes/ofw2018/agents/stats.php
+++ wp-content/themes/ofw2018/agents/stats.php
@@ -418,12 +418,6 @@
     );
     $members_for_this_month = new WP_Query($mftm_args);
 
-    $members_for_this_month_ids_list = [];
-
-    foreach ($members_for_this_month as $member_for_this_month) {
-        echo $member_for_this_month->post->ID.'<br>';
-    }
-
     //get all users registered this year
     $rtyui = array(
         'include'       => $all_registered_user_ids_list,
@@ -467,7 +461,7 @@
     echo '<div>Total Users Registered This Week By Agent ID '.$thisUser.' = '.count($registered_this_week_user_ids_list).'</div>';
     //echo '<div>Total Users That Are Member For This Week By Agent ID '.$thisUser.' = '.count($members_for_this_week_ids_list).'</div>';
     echo '<div>Total Users Registered This Month By Agent ID '.$thisUser.' = '.count($registered_this_month_user_ids_list).'</div>';
-    echo '<div>Total Users That Are Member For This Month By Agent ID '.$thisUser.' = '.count($members_for_this_month_ids_list).'</div>';
+    echo '<div>Total Users That Are Member For This Month By Agent ID '.$thisUser.' = '.count($$members_for_this_month).'</div>';
     echo '<div>Total Users Registered This Year By Agent ID '.$thisUser.' = '.count($registered_this_year_user_ids_list).'</div>';
     //echo '<div>Total Users That Are Member For This Year By Agent ID '.$thisUser.' = '.count($members_for_this_year_ids_list).'</div>';
 

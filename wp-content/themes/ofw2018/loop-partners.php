33491a55fd491423c4b643662d2cc46fa4c305ec
jujujuanna <joanastronomo@gmail.com>
2018-09-05 12:13:52 +0800

    partners archive

--- wp-content/themes/ofw2018/loop-partners.php
+++ wp-content/themes/ofw2018/loop-partners.php
@@ -3,16 +3,16 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<div class="col-sm-4 mb-4">
-		<?php $logo = the_post_thumbnail(); ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 				<div class="logo-cont transition d-flex">
+					<?php $logo = the_post_thumbnail(); ?>
 					<?php if (!empty($logo)) { ?>
 						<?php the_post_thumbnail('full', array('class' => 'img-fluid transition mx-auto d-block align-self-center')); ?>
 					<?php } else { ?>
 						<img src="<?php echo get_template_directory_uri(); ?>/img/no-logo.png" alt="<?php the_title(); ?>" class="img-fluid transition mx-auto d-block align-self-center"/>
 					<?php } ?>
 				</div>
-				<p><?php the_title(); ?></p>
+				<p class="text-center font-weight-bold red"><?php the_title(); ?></p>
 			</a>
 	</div>
 

86efbb32f9a5c1be99b484b2da6106731d8e440a
jesel@sakurasky.com <jesel@sakurasky.com>
2018-09-14 10:37:40 +0800

    test

--- wp-content/themes/ofw2018/members/sell-product.php
+++ wp-content/themes/ofw2018/members/sell-product.php
@@ -1,8 +1,20 @@
 <?php
 
 $current_user = wp_get_current_user();
-
-print_r($current_user);
-
+$post_meta = get_post_meta( 593 );
+print_r($post_meta);
 
 ?>
+
+<form>
+	<div class="row">
+		<div class="col-md-12 form-group">
+			<label>Product Name</label>
+			<input type="text" name="product_name" value="" placeholder="Product Name" class="form-control">
+		</div>
+		<div class="col-md-12 form-group">
+			<label>Product Description</label>
+			<textarea placeholder="Product Description" class="form-control"></texatea>
+		</div>
+	</div>
+</form>

c43ddc2578d8c5f7fe237248bb7fdff1b3262f74
jesel@sakurasky.com <jesel@sakurasky.com>
2018-09-14 14:55:38 +0800

    test

--- wp-content/themes/ofw2018/members/sell-product.php
+++ wp-content/themes/ofw2018/members/sell-product.php
@@ -48,7 +48,9 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
         $product = get_post($new_product);
         add_post_meta( $new_product, 'price', $price );
 
-        wp_set_post_terms( $new_product, $product_category_list, 'prod_cat', true );
+        //wp_set_post_terms( $new_product, $product_category_list, 'prod_cat', true );
+        print_r($new_product);
+        print_r($product_category );
 
         
 

c4e14a3afefae5b56e73a757edacb20ea9bd6505
jujujuanna <joanastronomo@gmail.com>
2018-09-20 13:16:33 +0800

    preview link

--- wp-content/themes/ofw2018/members/product-list.php
+++ wp-content/themes/ofw2018/members/product-list.php
@@ -42,7 +42,7 @@ if ($products->have_posts()):
 					<tr>
 						<th>
 							<!-- <?php if ($post->post_status == 'publish') { ?> -->
-							<a href="<?php echo $current_url.'?post_type=products&p='.$post->ID;.'&preview=true'. ?>">
+							<a href="<?php echo $current_url.'?post_type=products&p='.$post->ID.'&preview=true'.;?>">
 								
 							<!-- <?php } ?> -->
 							<?php 

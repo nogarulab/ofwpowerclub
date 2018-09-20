be5fae35fbfd8fbb0285d65feb7bb241f43f7da5
jujujuanna <joanastronomo@gmail.com>
2018-09-20 15:27:45 +0800

    permalink or perview

--- wp-content/themes/ofw2018/members/product-list.php
+++ wp-content/themes/ofw2018/members/product-list.php
@@ -42,9 +42,9 @@ if ($products->have_posts()):
 					<tr>
 						<th>
 							<?php if ($post->post_status == 'publish') { ?> 
-								<a href="<?php echo home_url().'?post_type=products&p='.$post->ID.'&preview=true';?>">
-							<?php } else { ?> 
 								<a href="<?php the_permalink(); ?>"></a>
+							<?php } else { ?> 
+								<a href="<?php echo home_url().'?post_type=products&p='.$post->ID.'&preview=true';?>">
 							<?php } ?>
 							<?php 
 								if ($sold == 'Sold') {

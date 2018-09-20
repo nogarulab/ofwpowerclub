c492e85b91e727cec440bf5f1c27332b77e86fb0
jujujuanna <joanastronomo@gmail.com>
2018-09-20 19:09:33 +0800

    edit prod

--- wp-content/themes/ofw2018/members/edit-product.php
+++ wp-content/themes/ofw2018/members/edit-product.php
@@ -3,27 +3,6 @@
 <?php get_header(); ?>
 
 
-<?php 
-		$the_query = new WP_Query(array('post_type'=>'page', 'pagename'=>'edit-my-product'));
-		while ( $the_query->have_posts() ) : $the_query->the_post();
-		$featuredImage = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); 
-	?>
-
-	<section class="page-banner py-5 white text-uppercase" style="background-image:url(<?php echo $featuredImage; ?>);">
-		<div class="container py-5">
-			<div class="row">
-				<div class="col text-center">
-					<h2 class="font-weight-bold "><?php wp_title(''); ?></h2>
-					<span class="bread-crumbs">Home // <a href="<?php the_permalink() ?>" class="yellow transition"><?php the_title(); ?></a></span>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<?php 
-		endwhile; wp_reset_query(); 
-	?>
-
 <div class="container edit-product">
 
 <?php

6f0dbf44061817385868b2850cde993f6574c5e6
jujujuanna <joanastronomo@gmail.com>
2018-09-20 20:10:00 +0800

    edit if published

--- wp-content/themes/ofw2018/members/product-list.php
+++ wp-content/themes/ofw2018/members/product-list.php
@@ -69,16 +69,9 @@ if ($products->have_posts()):
 							?>
 						</td>
 						<td>
-							<!-- <?php #if ($post->post_status == 'publish') { ?>
+							<?php if ($post->post_status == 'publish') { ?>
 								<a href="<?php #the_permalink(); ?>?edit=true">Edit</a>
-							<?php # } ?> -->
-							<?php $edit_post = add_query_arg( 'post', $post->ID, get_permalink( 690 + $_POST['_wp_http_referer'] ) ); ?>
-							<?php if ($post->post_status == 'publish') { ?> 
-								<a href="<?php the_permalink(); ?>?edit=true">
-							<?php } else { ?> 
-								<a href="<?php echo $edit_post; ?>">
-							<?php } ?>
-								Edit
+							<?php  } ?>
 							</a>
 							<a href="#" class="deletepost" data-toggle="modal" data-currenturl="<?php echo $current_url.'?id='.$post->ID; ?>" data-target="#deleteProdConfirmation">Delete</a>
 						</td>

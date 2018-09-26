75aff5201afc50b672dee36f3a76140fded50e20
jujujuanna <joanastronomo@gmail.com>
2018-09-26 13:05:24 +0800

    dot

--- wp-content/themes/ofw2018/partners/contact.php
+++ wp-content/themes/ofw2018/partners/contact.php
@@ -1,7 +1,7 @@
 <div class="user-dashboard-home">
 	<header>
 		<h3>Contact Administrator</h3>
-		<p>We are here to assist you</p>
+		<p>We are here to assist you.</p>
 	</header>
 <?php
 if ( is_user_logged_in() && current_user_can('partner') ) :
